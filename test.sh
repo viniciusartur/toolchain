2b1b1e729d9027b8d2177e23a66411796ccd6d93
Mag <mag@magwas.rulez.org>
2019-08-01 20:37:33 +0100

    test script, temporary version

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone -b feature/toolchain https://github.com/magwas/PDEngine.git
+sed -i 'sA^build_image:.*Abuild_image: kodekonveyor/toolchain:masterA' PDEngine/shippable.yml 
+cd PDEngine
+pwd
+DOCKER_IMAGE=kodekonveyor/toolchain:master ENTRYPOINT=/usr/local/toolchain/tools/Script ../inproject/tools/testenv
+
