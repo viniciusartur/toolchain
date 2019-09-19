3223749f0518f9e792b7262ba3f9a2fbb3918107
Mag <mag@magwas.rulez.org>
2019-09-19 06:43:46 +0100

    fail on constraint errors

--- rules.java
+++ rules.java
@@ -32,7 +32,10 @@ else #IS_PULL_REQUEST
 sonar: $(BEFORE_SONAR) sonarconfig buildreports
 	$(TOOLCHAINDIR)/tools/pullanalize
 
-createdocs: $(MODEL_BASENAME).compiled codedocs
+createdocs: $(MODEL_BASENAME).compiled codedocs checkdoc
+
+checkdoc: $(MODEL_BASENAME).consistencycheck
+	tools/checkDocErrors
 endif #IS_PULL_REQUEST
 
 sonarconfig:
